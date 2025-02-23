bd663b4b576a59a47e3dddbe97e774e714d5e45b
Navi <lunaqua@proton.me>
2025-02-23 16:41:03 +0000

    ... ver bump.. >~<

--- collabvm3_themes.user.js
+++ collabvm3_themes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CollabVM3 Themes
 // @namespace    https://github.com/Lunaqua/collabvm3_themes
-// @version      2025-02-23_3
+// @version      2025-02-23_4
 // @description  Themes for CollabVM 3
 // @author       navi4205
 // @match        https://computernewb.com/collab-vm/experimental-vm/
