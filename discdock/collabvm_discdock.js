6e2836fdee3055fdc2e914f5ae48a62cf6af4f8b
Navi <lunaqua@proton.me>
2025-03-21 14:44:22 +0000

    Ok things actually work now.

--- discdock/collabvm_discdock.js
+++ discdock/collabvm_discdock.js
@@ -7,17 +7,35 @@
 // @match        https://computernewb.com/collab-vm/
 // @match        https://computernewb.com/collab-vm/classic/
 // @match        https://computernewb.com/collab-vm/experimental-vm/
-// @resource cssInject  https://raw.githubusercontent.com/Lunaqua/collabvm3_themes/refs/heads/discdock/discdock.css
-// @resource htmlInject https://raw.githubusercontent.com/Lunaqua/collabvm3_themes/refs/heads/mdiscdock/discdock.html
+// @resource cssInject  https://raw.githubusercontent.com/Lunaqua/collabvm3_themes/refs/heads/discdock/discdock/discdock.css
+// @resource htmlInject https://raw.githubusercontent.com/Lunaqua/collabvm3_themes/refs/heads/discdock/discdock/discdock.html
 // @grant GM_getResourceText
 // ==/UserScript==
 
 // This is based (ripped off) from a similar concept in use at https://infinitemac.org/
 
+function loadCss(){
+    let dockCss = document.createElement("style");
+    const cssResText = GM_getResourceText("cssInject");
+    // Grab the CSS Resouce from github.
+    dockCss.innerHTML = cssResText;
+
+    document.getElementsByTagName('head')[0].appendChild(dockCss);
+}
+    
 function createDockTab(){
     const dockTab = document.createElement("div");
-    dockTab.id="discImageTab";
-    dockTab.classList.toggle("dock-tab");
-    
-    dockTab.innerHTML = "<p>Disc Images</p>"
+    const bodyTag = document.getElementsByTagName("body")[0];
+    dockTab.id="discImageTabContainer";
+    dockTab.classList.toggle("dock-tab-container");
+
+    dockTab.innerHTML = '<div id="discImageTab" class="dock-tab"><p>Disc Images</p></div>';
+    bodyTag.appendChild(dockTab);
 }
+
+function main(){
+    createDockTab();
+    loadCss();
+}
+
+main();
